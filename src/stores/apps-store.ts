98849413a044ad2714acd4e20dbd4100137b6da0
99-zziy <wldud8229@gmail.com>
2023-03-28 22:49:23 +0900

    feat: window open store 생성

---
+++ src/stores/apps-store.ts
@@ -0,0 +1,22 @@
+import { apps } from '@/data/apps/apps';
+import { atom } from 'recoil';
+
+export type appID = keyof typeof apps;
+
+export const openAppsState = atom<Record<appID, boolean>>({
+    key: 'openAppsState',
+    default: {
+        finder: false,
+        calculator: false,
+        calendar: false,
+        github: false,
+        mail: false,
+        memo: false,
+        message: false,
+        notion: false,
+        'photo-booth': false,
+        photo: false,
+        safari: false,
+        'system-preferences': false,
+    },
+});
