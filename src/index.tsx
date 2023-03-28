d127b4a3e6022ff33ef58557567f848dab4f2bdc
99-zziy <wldud8229@gmail.com>
2023-03-28 22:48:34 +0900

    feat: recoil 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from '@/styles/global-styles';
 import DesktopPage from '@/pages/Desktop';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
         <GlobalStyle />
-        <DesktopPage />
+        <RecoilRoot>
+            <DesktopPage />
+        </RecoilRoot>
     </React.StrictMode>
 );
