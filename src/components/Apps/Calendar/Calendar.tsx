6583703c47b4c68b646c4f2ad41ca48170b0aecc
wonho <wonho1401@naver.com>
2023-04-18 21:44:16 +0900

    feat: 캘린더 헤더 생성

--- src/components/Apps/Calendar/Calendar.tsx
+++ src/components/Apps/Calendar/Calendar.tsx
@@ -1,5 +1,89 @@
+import { calendarAppStore } from '@/stores/calendar-store';
 import styled from '@emotion/styled';
+import { format } from 'date-fns';
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 
-const Calendar = () => {};
+type CalendarView = 'day' | 'week' | 'month' | 'year';
+
+const Calendar = () => {
+    const [view] = useState<CalendarView>('month');
+    const [selectedDate, setSelectedDate] = useRecoilState(calendarAppStore);
+
+    return (
+        <CalendarContainer>
+            <CalendarHeader>
+                <TodayYearMonth>
+                    <Year>{format(selectedDate, 'yyyy')}년</Year>
+                    <Month>{format(selectedDate, 'M')}월</Month>
+                </TodayYearMonth>
+                <MoveMonthContainer>
+                    <PrevMonthButton>&lt;</PrevMonthButton>
+                    <TodayButton>오늘</TodayButton>
+                    <NextMonthButton>&gt;</NextMonthButton>
+                </MoveMonthContainer>
+            </CalendarHeader>
+            <MonthlyView></MonthlyView>
+        </CalendarContainer>
+    );
+};
+
+const CalendarContainer = styled.div``;
+
+const CalendarHeader = styled.div`
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const MonthlyView = styled.div``;
+
+const TodayYearMonth = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Year = styled.div`
+    font-size: 2rem;
+    font-weight: bold;
+    color: rgb(222, 221, 221);
+`;
+
+const Month = styled.div`
+    padding-left: 5px;
+    font-size: 2rem;
+    font-weight: bold;
+    color: rgb(222, 221, 221);
+`;
+
+const MoveMonthContainer = styled.div``;
+
+const PrevMonthButton = styled.button`
+    background-color: rgb(97, 86, 89);
+    border-radius: 0.375rem;
+    border-style: none;
+    color: white;
+    margin-right: 0.1rem;
+    height: 20px;
+`;
+
+const TodayButton = styled.button`
+    background-color: rgb(97, 86, 89);
+    border-radius: 0.375rem;
+    border-style: none;
+    color: white;
+    width: 50px;
+    height: 20px;
+`;
+
+const NextMonthButton = styled.button`
+    background-color: rgb(97, 86, 89);
+    border-radius: 0.375rem;
+    border-style: none;
+    color: white;
+    margin-left: 0.1rem;
+    height: 20px;
+`;
 
 export default Calendar;
