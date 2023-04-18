bd15751ea5b9fc059f66f96a55b365d1c2d81be6
wonho <wonho1401@naver.com>
2023-04-18 21:44:04 +0900

    refactor: 캘린더 width 수정

--- src/data/apps/calendar.ts
+++ src/data/apps/calendar.ts
@@ -2,7 +2,9 @@ import { AppConfig } from '@/data/apps/apps';
 
 export const calendar: AppConfig = {
     title: 'calendar',
-    top: 300,
-    left: 750,
+    top: 50,
+    left: 350,
+    width: 900,
     label: '캘린더',
+    backgroundColor: 'rgba(43, 33, 30, 1)',
 };
