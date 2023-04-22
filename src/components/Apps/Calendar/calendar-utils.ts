817f9220c6bda314b128d696e26a2ba6f7ae5bf2
wonho <wonho1401@naver.com>
2023-04-22 18:39:06 +0900

    del: calendar utils 삭제

--- src/components/Apps/Calendar/calendar-utils.ts
+++
@@ -1,6 +0,0 @@
-import { getDay, getMonth, getYear, startOfMonth } from 'date-fns';
-import { DAYS, LEAP_DAYS, DAY_OF_THE_WEEK } from './constants';
-
-export function isLeap(year: number) {
-    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-}
